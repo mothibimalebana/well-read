0b7010df18b0dddb3c3df02a3aabd015d1fd037d
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-30 23:19:58 +0200

    Check if each isbn entry is unique

--- backend/populatedb.py
+++ backend/populatedb.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 # load data from csv
 data = pd.read_csv("Books.csv")
+
+# check if each isbn entry is unique
+print(data['ISBN'].is_unique)
+
+# load first 20 rows into a csv file
+create_first_20_csv = data.head(20).to_csv("first_20.csv", index=False)
