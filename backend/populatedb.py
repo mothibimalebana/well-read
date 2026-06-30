578b071d3c7ff1b2c3427bc5f543c56c5a9eb5c2
mothibi malebana <mothibi.mm12@gmail.com>
2026-07-01 00:16:30 +0200

    Group by authors and get count of each feature
    This will allows for normalization, exposing the relation between different features

--- backend/populatedb.py
+++ backend/populatedb.py
@@ -3,5 +3,5 @@ import pandas as pd
 # load data from csv
 data = pd.read_csv("Books.csv")
 
-# check if each book-title entry is unique
-print(data['Book-Title'].is_unique)
+# Group by authors and get count of each feature
+grouped_authors = data.groupby(['Book-Author']).count().to_csv("group_by_authors.csv")
