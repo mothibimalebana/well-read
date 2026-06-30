04672abd414244357fde37b11b9293a69fcff74a
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-30 23:21:37 +0200

    Check if each book-title entry is unique

--- backend/populatedb.py
+++ backend/populatedb.py
@@ -3,5 +3,5 @@ import pandas as pd
 # load data from csv
 data = pd.read_csv("Books.csv")
 
-# check if each isbn entry is unique
+# check if each book-title entry is unique
 print(data['Book-Title'].is_unique)
