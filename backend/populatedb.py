b0da3e14c6215867e8983c2abeea3b3c93fbf143
mothibi malebana <mothibi.mm12@gmail.com>
2026-07-01 00:20:52 +0200

    Group by ISBN and get count of each feature

--- backend/populatedb.py
+++ backend/populatedb.py
@@ -3,5 +3,5 @@ import pandas as pd
 # load data from csv
 data = pd.read_csv("Books.csv")
 
-# Group by authors and get count of each feature
-grouped_authors = data.groupby(['Book-Author']).count().to_csv("group_by_authors.csv")
+# Group by ISBN and get count of each feature
+grouped_authors = data.groupby(['ISBN']).count().to_csv("group_by_isbn.csv")
