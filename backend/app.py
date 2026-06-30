b87d501b177062775ba5b2d65e354d954fadd618
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-30 20:21:45 +0200

    Connect to database (mydatabase)

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import psycopg
 
 app = Flask(__name__)
 
 # connect to database
+with psycopg.connect("postgresql://myuser:mypassword@localhost:5432/mydatabase") as conn:
+    print(conn)
 
 if __name__ == "__main__":
     app.run(debug=True)
