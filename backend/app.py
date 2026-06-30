afdd127fe1db88a94073a9592ec23ab93287ff0d
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-30 22:19:13 +0200

    Create populatedb script
    The script will populate the postgres database with values from the book.csv

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 # connect to database
 with psycopg.connect("postgresql://myuser:mypassword@localhost:5432/mydatabase") as conn:
-    print(conn)
+    with conn.cursor() as cur:
+        cur.execute("""
+            select * from test;
+            """)
+        print(cur.fetchone())
 
 if __name__ == "__main__":
     app.run(debug=True)
